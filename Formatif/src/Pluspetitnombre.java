d10d7fec2178cc8995981e3798ab47e7a7e8a049
Liam <Liam@10.5.1.244>
2020-12-11 14:12:31 -0500

    11 Décembre

---
+++ Formatif/src/Pluspetitnombre.java
@@ -0,0 +1,15 @@
+/*
+* Author : Liam
+*Date : 11 Dec, 2020
+*/
+public class Pluspetitnombre
+	{
+
+		public static void main(String[] args)
+			{
+				int[]monTab= {3,5,1,8,99,234};
+				int a =MethodesTableaux.plusPetitIndex(monTab);
+				System.out.println(a);
+			}
+
+	}
---
+++ Formatif/src/SommeTableau.java
@@ -0,0 +1,17 @@
+/*
+* Author : Liam
+*Date : 11 Dec, 2020
+*/
+public class SommeTableau
+	{
+
+		public static void main(String[] args)
+			{
+				int[] array = {11, 22, 33, 44, 55, 66, 77, 88, 99, 10};
+				int somme = 0;
+				for(int numero :array);
+				
+				System.out.println("Voici la somme de toutes le éléments:");
+			}
+
+	}
